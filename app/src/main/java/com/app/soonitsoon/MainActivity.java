0f9276d3e7b79fd1532bd4a98c3a293673f2ee9e
YoonSoo Kim <fhkaldh@naver.com>
2020-10-08 18:10:48 +0900

    GPS 테스트 버튼 및 KakaoMap 띄우기

--- app/src/main/java/com/app/soonitsoon/MainActivity.java
+++ app/src/main/java/com/app/soonitsoon/MainActivity.java
@@ -1,13 +1,76 @@
 package com.app.soonitsoon;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+    private Activity mainActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        // key hash
+//        Log.e("getKeyHash", ""+getKeyHash(MainActivity.this));
+
+        Button btn = findViewById(R.id.gpsBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GpsTracker gpsTracker = new GpsTracker(mainActivity);
+                double latitute = gpsTracker.getLatitude();
+                double longitude = gpsTracker.getLongitude();
+
+                String toastStr = "latitude : " + Double.toString(latitute) + ", longitude : " + Double.toString(longitude);
+
+                Toast.makeText(getApplicationContext(), toastStr, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    // key hash
+    public static String getKeyHash(final Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            if (packageInfo == null)
+                return null;
+
+            for (Signature signature : packageInfo.signatures) {
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return android.util.Base64.encodeToString(md.digest(), android.util.Base64.NO_WRAP);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
