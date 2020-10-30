28b34cc2136482fe4aed4e3776a82dda1dca5695
YoonSoo Kim <fhkaldh@naver.com>
2020-10-30 19:31:09 +0900

    수정 - UI Navigation Bar 생성 및 컬러적용

--- app/src/main/java/com/app/soonitsoon/MainActivity.java
+++ app/src/main/java/com/app/soonitsoon/MainActivity.java
@@ -1,13 +1,20 @@
 package com.app.soonitsoon;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -15,6 +22,7 @@ import android.widget.Toast;
 
 import com.app.soonitsoon.timeline.DateNTime;
 import com.app.soonitsoon.timeline.GpsTracker;
+import com.google.android.material.navigation.NavigationView;
 
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
@@ -23,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
     public Activity mainActivity = this;
     private Intent mBackgroundServiceIntent;
     private BackgroundService mBackgroundService;
+
+    private DrawerLayout mDrawerLayout;
+    private Context context = this;
+
+    Toolbar toolbar;
+    Fragment timelineFragment, test1Fragment, test2Fragment, test3Fragment;
 
 
     @Override
@@ -34,6 +48,54 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
+
+        // 상단 바
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_icon); //뒤로가기 버튼 이미지 지정
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        timelineFragment = new TimelineFragment();
+        test1Fragment = new Test1Fragment();
+        test2Fragment = new Test2Fragment();
+        test3Fragment = new Test3Fragment();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+                String title = menuItem.getTitle().toString();
+
+                if(id == R.id.nav_item_home){
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.nav_item_timeline){
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.nav_item_test1){
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.nav_item_test2){
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.nav_item_test3){
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+
+
+
 
         final MapView mapView = new MapView(this);
         ViewGroup mapViewContainer = findViewById(R.id.map_view);
@@ -121,5 +183,16 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), toastStr, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/app/soonitsoon/TimelineFragment.java
@@ -0,0 +1,19 @@
+package com.app.soonitsoon;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TimelineFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) getLayoutInflater().inflate(R.layout.timeline_layout, container, false);
+        return rootView;
+    }
+}
