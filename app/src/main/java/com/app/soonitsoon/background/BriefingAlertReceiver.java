dd61daf73412f16237e42b3dc303fa9f6f1aaf36
kyeolhee <ggood929@naver.com>
2020-12-11 14:54:25 +0900

    하루 한번 브리핑 되나?

--- app/src/main/java/com/app/soonitsoon/background/BriefingAlertReceiver.java
+++ app/src/main/java/com/app/soonitsoon/background/BriefingAlertReceiver.java
@@ -1,16 +1,17 @@
 package com.app.soonitsoon.background;
 
+import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import com.app.soonitsoon.Alert;
 
-public class BriefingAlertReveiver extends BroadcastReceiver {
+public class BriefingAlertReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Alert briefingAlert = new Alert(context, );
-
+        Alert briefingAlert = new Alert(context, (Application) context.getApplicationContext());
+        briefingAlert.sendBriefingAlert();
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
